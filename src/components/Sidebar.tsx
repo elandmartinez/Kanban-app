e1d021201441abeaed57990593194a41c7be5845
Elias Martinez <elandmartinez+github@gmail.com>
2025-07-06 23:53:47 -0500

    WIP: working on making the sidebar disappear and get the main section component to occupy the the wholle screen width

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,9 +25,9 @@ export default function SideBar () {
   }
 
   return (
-    <aside ref={sidebarRef} className="hidden relative w-0 rounded-xl bg-darkGrey text-mediumGrey text-[0.85rem] font-bold flex-col justify-between items-center py-4 transition-all duration-200
+    <aside ref={sidebarRef} className="relative w-[300px] rounded-xl bg-darkGrey text-mediumGrey text-[0.85rem] font-bold flex-col justify-between items-center py-4 transition-all duration-200
       sm:h-[92vh] sm:flex sm:rounded-none">
-      <nav className="absolute w-full text-start p-4 pl-0">
+      <nav className="w-full text-start p-4 pl-0">
         <h3 className="text-[0.82rem] tracking-[0.11em] font-bold mb-4 pl-6">
           ALL BOARDS (3)
         </h3>
@@ -48,7 +48,7 @@ export default function SideBar () {
           + Create New Board
         </button>
       </nav>
-      <div className="absolute w-[90%] mb-4 justify-between bg-veryDarkGrey rounded-lg align-middle gap-[0.5rem] p-3">
+      <div className="w-[90%] mb-4 flex justify-between bg-veryDarkGrey rounded-lg align-middle gap-[0.5rem] p-3">
         <Icon SvgComponent={sunIcon} classname="w-6 h-6" />
           <Switch className="mx-4" />
         <Icon SvgComponent={moonIcon} classname="w-6 h-6" />
