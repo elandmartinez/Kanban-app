8ba07df72d5fd31b2663db6e153a376906102735
Elias Martinez <elandmartinez+github@gmail.com>
2025-07-17 12:37:54 -0500

    working on subtask update

--- src/components/TaskDialog.tsx
+++ src/components/TaskDialog.tsx
@@ -1,14 +1,31 @@
 import { Checkbox } from "../components/ui/checkbox"
-import { Task } from "@/appState/slices/taskSlice"
+import { editTask, Task } from "../appState/slices/taskSlice"
 import { DialogTitle } from "@radix-ui/react-dialog"
+import { useDispatch } from "react-redux"
 
 interface TaskDialogProps {
   taskData: Task | null
 }
 
 export default function TaskDialog ({ taskData }: TaskDialogProps) {  
+  const dispatch = useDispatch()
   const subtasksCompleted = taskData?.subtasks.reduce((accumulator, current) => (current.done ? accumulator+1 : 0), 0)
   const totalSubtasks = taskData?.subtasks.length
+
+  function updateSubtasksStatus(subtaskIndex: number, newDoneStatus: boolean) {
+    if (taskData) {
+      // Create a new copy of the subtasks array
+      const updatedSubtasks = taskData.subtasks.map((subtask, index) =>
+        index === subtaskIndex ? { ...subtask, done: newDoneStatus } : subtask
+      );
+  
+      // Create a new taskData object with the updated subtasks
+      const newTaskData = { ...taskData, subtasks: updatedSubtasks };
+  
+      // Dispatch the updated task data
+      dispatch(editTask({ id: taskData.id, newTask: newTaskData }));
+    }
+  }
 
   return (
     <div className="flex flex-col gap-4 text-start">
@@ -21,7 +38,7 @@ export default function TaskDialog ({ taskData }: TaskDialogProps) {
         {
           taskData?.subtasks.map((subtask, index) => (
             <div key={index} className="bg-background rounded-sm p-2 flex items-center text-secondaryTextColor text-[0.75rem] font-bold">
-              <Checkbox id="subtask" className="mr-2" />
+              <Checkbox onClick={() => updateSubtasksStatus(index, !subtask.done) } checked={subtask.done} id="subtask" className="mr-2" />
               <p className="w-full">{subtask.name}</p>
             </div>
           ))
